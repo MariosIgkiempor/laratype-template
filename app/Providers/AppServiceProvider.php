51be203ae75114c97577bbbd0ae6836938ad6a78
Mario Igkiempor <marioigkiempor@gmail.com>
2025-01-09 22:06:50 +0000

    feat: Add sensible defaults to AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
@@ -21,6 +27,50 @@ final class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configureDates();
+        $this->configureCommands();
+        $this->configureModels();
+        $this->configureUrl();
+        $this->configureVite();
+    }
+
+    /**
+     * Configure the app's models
+     */
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict();
+        Model::unguard();
+    }
+
+    /**
+     * Configure the app's commands
+     */
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            prohibit: app()->isProduction()
+        );
+    }
+
+    /**
+     * Configure the application's URL.
+     */
+    private function configureUrl(): void
+    {
+        URL::forceScheme('https');
+    }
+
+    /**
+     * Configure the application's Vite.
+     */
+    private function configureVite(): void
+    {
+        Vite::usePrefetchStrategy('aggressive');
+    }
+
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
     }
 }
